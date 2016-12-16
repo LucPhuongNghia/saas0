33482f40cd0b42de7fc080fcab1ec75037b5d011
nghia <nghialp@tocdep.vn>
2016-12-16 11:17:41 +0000

    Edit update user profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,8 @@
 class ProfilesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :only_current_user
+  
+  
   def new
     @profile = Profile.new
   end
@@ -8,14 +12,38 @@ class ProfilesController < ApplicationController
     @profile = @user.build_profile(profile_params)
     if @profile.save
       flash[:notice] = "Profile updated"
-      redirect_to user_path(params[:user_id])
+      redirect_to user_path(id: params[:user_id])
     else
       render :new
+    end
+  end
+  
+  #GET '/users/:user_id/profile/edit'
+  def edit
+    @user = User.find(params[:user_id])
+    @profile = @user.profile
+  end
+  
+  #PUT '/users/:user_id/profile'
+  def update
+    @user = User.find(params[:user_id])
+    @profile = @user.profile
+    if @profile.update(profile_params)
+      flash[:notice] = "Profile updated successfully"
+      redirect_to user_path(@user) #id: @user.id
+    else
+      render :edit
     end
   end
   
   private
     def profile_params
       params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
+    end
+    
+    def only_current_user
+      @user = User.find(params[:user_id])
+      flash[:notice] = "Only profile owner can edit his/her profile"
+      redirect_to root_path unless @user == current_user
     end
 end
